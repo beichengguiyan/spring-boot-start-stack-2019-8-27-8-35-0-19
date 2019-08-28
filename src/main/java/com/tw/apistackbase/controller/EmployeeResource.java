ae1ba58df261d1b857037a6a2f6a811962925827
beichengguiyan <chihaotian@zybank.com.cn>
2019-08-28 16:38:04 +0800

    # WARNING: head commit changed in the meantime
    test

--- src/main/java/com/tw/apistackbase/controller/EmployeeResource.java
+++ src/main/java/com/tw/apistackbase/controller/EmployeeResource.java
@@ -1,13 +1,17 @@
 package com.tw.apistackbase.controller;
 
-
+import java.security.Provider.Service;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tw.apistackbase.model.Employee;
@@ -15,36 +19,39 @@ import com.tw.apistackbase.model.Employee;
 @RestController
 @RequestMapping("/employees")
 public class EmployeeResource {
-    @GetMapping()
-    public ResponseEntity<List<Employee>> getAll() {
-    	List<Employee> employees = new ArrayList<Employee>();
-    	Employee employeeA = new Employee(0,"Xiaoming",20,"Male");
-    	Employee employeeB = new Employee(1,"Xiaohong",19,"Female");
-    	Employee employeeC = new Employee(2,"Xiaozhi",15,"Male");
-    	Employee employeeD = new Employee(3,"Xiaogang",16,"Male");
-    	Employee employeeE = new Employee(4,"Xiaoxia",15,"Female");
-    	employees.add(employeeA);
-    	employees.add(employeeB);
-    	employees.add(employeeC);
-    	employees.add(employeeD);
-    	employees.add(employeeE);
-        return ResponseEntity.ok(employees);
-    }
-    
-    @GetMapping(path = "/{employeeId}")
-    public ResponseEntity<Employee> getEmployee(@PathVariable int employeeId) {
-    	List<Employee> employees = new ArrayList<Employee>();
-    	Employee employeeA = new Employee(0,"Xiaoming",20,"Male");
-    	Employee employeeB = new Employee(1,"Xiaohong",19,"Female");
-    	Employee employeeC = new Employee(2,"Xiaozhi",15,"Male");
-    	Employee employeeD = new Employee(3,"Xiaogang",16,"Male");
-    	Employee employeeE = new Employee(4,"Xiaoxia",15,"Female");
-    	employees.add(employeeA);
-    	employees.add(employeeB);
-    	employees.add(employeeC);
-    	employees.add(employeeD);
-    	employees.add(employeeE);
-        return ResponseEntity.ok(employees.get(employeeId));
-    }
+	private static List<Employee> employees = new ArrayList<Employee>() {{
+		add(new Employee(0, "Xiaoming", 20, "Male"));
+		add(new Employee(1, "Xiaohong", 19, "Female"));
+		add(new Employee(2, "Xiaozhi", 15, "Male"));
+		add(new Employee(3, "Xiaogang", 16, "Male"));
+		add(new Employee(4, "Xiaoxia", 15, "Female"));
+	}};
+	
+	@GetMapping()
+	public ResponseEntity<List<Employee>> getAll() {
+		return ResponseEntity.ok(employees);
+	}
 
+	@GetMapping(path = "/{employeeId}")
+	public ResponseEntity<Employee> getEmployee(@PathVariable int employeeId) {
+		return ResponseEntity.ok(employees.get(employeeId));
+	}
+
+	@GetMapping(path = "/name")
+	public ResponseEntity<List<Employee>> getEmployeeByName(@RequestParam(name = "name",required=false) String name) {
+		List<Employee> employeesResult = new ArrayList<Employee>();
+		for (Employee employee : employees) {
+			if (employee.getName().contains(name)) {
+				employeesResult.add(employee);
+			}
+		}
+		return ResponseEntity.ok(employeesResult);
+	}
+
+	@PostMapping(consumes = "application/json")
+	public ResponseEntity<List<Employee>> addEmployee(@RequestBody Employee employee) {
+		employees.add(employee);
+		return ResponseEntity.status(HttpStatus.CREATED).build();
+	}
+	
 }
--- src/main/java/com/tw/apistackbase/model/Employee.java
+++ src/main/java/com/tw/apistackbase/model/Employee.java
@@ -5,6 +5,8 @@ public class Employee {
 	private String name;
 	private int age;
 	private String gender;
+	public Employee() {
+	}
 	public Employee(int id, String name, int age, String gender) {
 		this.id = id;
 		this.name = name;
